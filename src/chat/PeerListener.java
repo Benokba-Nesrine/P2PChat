70b16cb733a5184acbd45021bda1b33e9973b0cf
arija <boukhalfa.aridj01@gmail.com>
2025-12-05 15:36:52 +0100

    Update PeerListener.java

--- src/chat/PeerListener.java
+++ src/chat/PeerListener.java
@@ -1,13 +1,7 @@
-package chat; 
-// This file is inside the "chat" package (same folder as your other classes).
+package chat;
 
-// An interface is like a "contract" that says:
-// "Any class that implements this must provide the onPeerConnected method."
+import P2P.chat.Peer;
+
 public interface PeerListener {
-
-    // This method will be called whenever a new peer connects.
-    // ConnectionManager will run this method automatically.
-    //
-    // 'peer' is the new Peer object representing the connected user.
     void onPeerConnected(Peer peer);
 }
